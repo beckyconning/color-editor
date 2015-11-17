fa4aa790db9d9a9497067896ed03a6a592f89e5e
Becky Conning <becky.conning@icloud.com>
2015-11-17 20:03:57 +0000

    Updated menu dep

--- src/ColorEditorMenu/Component/State.purs
+++ src/ColorEditorMenu/Component/State.purs
@@ -2,6 +2,8 @@ module ColorEditorMenu.Component.State where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
+
 import Color.Component.Query (ColorQuery(..))
 import Halogen.Menu.Component.State (Menu(), makeMenu)
 
@@ -10,9 +12,11 @@ colorEditorMenu = makeMenu
   [ { label: "Color"
     , submenu:
         [ { label: "Load color"
+          , shortcutLabel: Nothing
           , value: LoadColor unit
           }
         , { label: "Save color"
+          , shortcutLabel: Nothing
           , value: SaveColor unit
           }
         ]
@@ -20,21 +24,27 @@ colorEditorMenu = makeMenu
   , { label: "Edit"
     , submenu:
         [ { label: "Increase red"
+          , shortcutLabel: Nothing
           , value: IncreaseRed unit
           }
         , { label: "Decrease red"
+          , shortcutLabel: Nothing
           , value: DecreaseRed unit
           }
         , { label: "Increase green"
+          , shortcutLabel: Nothing
           , value: IncreaseGreen unit
           }
         , { label: "Decrease green"
+          , shortcutLabel: Nothing
           , value: DecreaseGreen unit
           }
         , { label: "Increase blue"
+          , shortcutLabel: Nothing
           , value: IncreaseBlue unit
           }
         , { label: "Decrease blue"
+          , shortcutLabel: Nothing
           , value: DecreaseBlue unit
           }
         ]
